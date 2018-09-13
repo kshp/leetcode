5547baec1d65c3f610717a704086a3d8b493593f
pavlo-koshla <pavlo.koshla@edutone.com>
2018-09-13 11:56:04 +0300

    Arrays: 66 Plus One

---
+++ src/main/java/com/pk/leetcode/arrays/PlusOne_66.java
@@ -0,0 +1,27 @@
+package com.pk.leetcode.arrays;
+
+public class PlusOne_66 {
+
+    public static int[] plusOne(int[] digits) {
+        if (digits.length==0)
+            return digits;
+
+        int i = digits.length - 1;
+        digits[i] = digits[i] + 1;
+        while (digits[i] == 10) {
+            if (i == 0)
+                return resize(digits);
+            digits[i] = 0;
+            i--;
+            digits[i] = digits[i] + 1;
+        }
+
+        return digits;
+    }
+
+    private static int[] resize(int[] digits) {
+        int[] arr = new int[digits.length + 1];
+        arr[0] = 1;
+        return arr;
+    }
+}
