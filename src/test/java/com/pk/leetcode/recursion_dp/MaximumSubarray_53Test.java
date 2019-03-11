6013f18d067af1e0e921def9967370aba91ac8d0
pavlo-koshla <pavlo.koshla@edutone.com>
2019-03-11 23:50:36 +0200

    53(E) Maximum Subarray - DP O(n)

---
+++ src/test/java/com/pk/leetcode/recursion_dp/MaximumSubarray_53Test.java
@@ -0,0 +1,16 @@
+package com.pk.leetcode.recursion_dp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MaximumSubarray_53Test {
+
+    private int[] arr1 = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    private int result1 = 6;
+
+    @Test
+    public void maxSubArray() {
+        assertEquals(result1, new MaximumSubarray_53().maxSubArray(arr1));
+    }
+}
