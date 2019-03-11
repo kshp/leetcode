9123080f0ab6dce21cceaa84244681ba99e7c4ce
pavlo-koshla <pavlo.koshla@edutone.com>
2019-03-11 23:51:19 +0200

    formatting fix

--- src/main/java/com/pk/leetcode/recursion_dp/MaximumSubarray_53.java
+++ src/main/java/com/pk/leetcode/recursion_dp/MaximumSubarray_53.java
@@ -5,8 +5,8 @@ public class MaximumSubarray_53 {
     public int maxSubArray(int[] nums) {
         int curr = nums[0];
         int max = nums[0];
-        for (int i=1; i<nums.length; i++){
-            curr = curr > 0 ? curr+nums[i] : nums[i];
+        for (int i = 1; i < nums.length; i++) {
+            curr = curr > 0 ? curr + nums[i] : nums[i];
             max = Math.max(curr, max);
         }
         return max;
