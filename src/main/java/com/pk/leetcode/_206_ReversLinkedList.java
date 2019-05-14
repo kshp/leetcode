6bc8f19532c89ce91e4a010d3ec7676d01416cc4
pavlo-koshla <pavlo.koshla@edutone.com>
2019-05-14 10:35:17 +0300

    206(E) Reverse Linked List

---
+++ src/main/java/com/pk/leetcode/_206_ReversLinkedList.java
@@ -0,0 +1,30 @@
+package com.pk.leetcode;
+
+import com.pk.leetcode.utils.ListNode;
+
+public class _206_ReversLinkedList {
+
+    public ListNode reverseListIterative(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+
+        return newHead;
+    }
+
+    public ListNode reverseListRecursive(ListNode head) {
+        return reverseListInt(head, null);
+    }
+
+    private ListNode reverseListInt(ListNode head, ListNode newHead) {
+        if (head == null)
+            return newHead;
+        ListNode next = head.next;
+        head.next = newHead;
+        return reverseListInt(next, head);
+    }
+}
---
+++ src/test/java/com/pk/leetcode/_206_ReversLinkedListTest.java
@@ -0,0 +1,41 @@
+package com.pk.leetcode;
+
+import com.pk.leetcode.utils.ListNode;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class _206_ReversLinkedListTest {
+
+    private ListNode node;
+    private ListNode result1;
+
+    @Before
+    public void init() {
+        node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+
+        result1 = new ListNode(4);
+        result1.next = new ListNode(3);
+        result1.next.next = new ListNode(2);
+        result1.next.next.next = new ListNode(1);
+    }
+
+    @Test
+    public void reverseListRecursive() {
+        _206_ReversLinkedList instance = new _206_ReversLinkedList();
+        ListNode listNode = instance.reverseListRecursive(node);
+        assertTrue(ListNode.equalNodes(result1, listNode));
+    }
+
+    @Test
+    public void reverseListIterative() {
+        _206_ReversLinkedList instance = new _206_ReversLinkedList();
+        ListNode listNode = instance.reverseListIterative(node);
+        assertTrue(ListNode.equalNodes(result1, listNode));
+    }
+}
